e3e2413e717c89e678d5e1231f0601b265b464ee
grodnik <jbarnes@dbasystems.com>
2024-03-10 09:56:01 -0500

    Initial Solution Setup

---
+++ Pantry/Pantry.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pantry.WebUI.Startup))]
+namespace Pantry.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
