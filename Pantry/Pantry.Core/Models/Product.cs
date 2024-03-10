50ffaf285bc65fd4e3a4ef7a1b47b326b35cd275
grodnik <jbarnes@dbasystems.com>
2024-03-10 13:01:51 -0500

    Product model and InMemory Product Repository Built

---
+++ Pantry/Pantry.Core/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pantry.Core.Models
+{
+  public class Product
+  {
+    public string ID {  get; set; }
+
+    [StringLength(30)]
+    [DisplayName("Item Name")]
+    public string Name { get; set; }
+
+    public string Type { get; set; }              /* Pantry/Kitchen/Meal */
+
+    public string Category { get; set; }          /* Detail of Type */
+
+    [Range(0,100)]
+    public decimal Price { get; set; }
+
+    public Product()
+    {
+      this.ID = Guid.NewGuid().ToString();
+    }
+  }
+}
