a1284208a7623198556cbe178e5c54a021cab6ea
grodnik <jbarnes@dbasystems.com>
2024-04-07 12:16:39 -0500

    Added Entity Framework

---
+++ Pantry/Pantry.DataAccess.SQL/Migrations/202404071616066_Initial.cs
@@ -0,0 +1,41 @@
+﻿namespace Pantry.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(maxLength: 30),
+                        Type = c.String(),
+                        Category = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.ProductTypes",
+                c => new
+                    {
+                        ID = c.String(nullable: false, maxLength: 128),
+                        Type = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.ProductTypes");
+            DropTable("dbo.Products");
+        }
+    }
+}
